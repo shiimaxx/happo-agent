257fbb525e60a84b5ff23d857a6e3b71ee28a38c
shiimaxx <yoshima@shiimaxx.com>
2019-01-18 17:23:59 +0900

    Move main logic to `listAliases`

--- command/list_aliases.go
+++ command/list_aliases.go
@@ -4,47 +4,61 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"strings"
 
 	"github.com/codegangsta/cli"
 	"github.com/heartbeatsjp/happo-agent/halib"
 	"github.com/heartbeatsjp/happo-agent/util"
+	"github.com/pkg/errors"
 )
 
 // CmdListAliases list aliases
 func CmdListAliases(c *cli.Context) error {
 	bastionEndpoint := c.String("bastion-endpoint")
+	agName := c.String("autoscaling_group_name")
+	listAll := c.Bool("all")
 
-	res, err := util.RequestToAutoScalingAPI(bastionEndpoint)
+	out, err := listAliases(bastionEndpoint, agName, listAll)
 	if err != nil {
 		return cli.NewExitError(err.Error(), 1)
+	}
+
+	fmt.Println(out)
+
+	return nil
+}
+
+func listAliases(bastionEndpoint, agName string, listAll bool) (string, error) {
+	res, err := util.RequestToAutoScalingAPI(bastionEndpoint)
+	if err != nil {
+		return "", err
 	}
 
 	var autoScalingResponse halib.AutoScalingResponse
 	data, err := ioutil.ReadAll(res.Body)
 	res.Body.Close()
 	if err != nil {
-		return cli.NewExitError(err.Error(), 1)
+		return "", err
 	}
 	if err := json.Unmarshal(data, &autoScalingResponse); err != nil {
-		return cli.NewExitError(err.Error(), 1)
+		return "", err
 	}
 
 	if len(autoScalingResponse.AutoScaling) < 1 {
-		cli.NewExitError("Missing auto scaling group", 1)
+		return "", errors.New("Missing auto scaling group")
 	}
 
-	agName := c.String("autoscaling_group_name")
-	listAll := c.Bool("all")
-
+	var out string
 	for _, a := range autoScalingResponse.AutoScaling {
 		if agName == "" || agName == a.AutoScalingGroupName {
 			for _, i := range a.Instances {
 				if listAll || i.InstanceData.IP != "" {
-					fmt.Println(i.Alias + "," + i.InstanceData.IP + "," + i.InstanceData.InstanceID)
+					out = out + fmt.Sprintln(i.Alias+","+i.InstanceData.IP+","+i.InstanceData.InstanceID)
 				}
 			}
 		}
 	}
+	out = strings.TrimSuffix(out, "\n")
 
-	return nil
+	return out, nil
 }
